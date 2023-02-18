edc12f1aa2bff8db3ef7ef743af94f7c8ff0ff61
Thomas Gauges <thomas.gauges@gmail.com>
2023-02-18 17:13:27 +0100

    feat: add indices to database schema

--- src-tauri/src/migrator.rs
+++ src-tauri/src/migrator.rs
@@ -1,12 +1,24 @@
 use sea_orm_migration::prelude::*;
 
 mod m20230113_000001_init;
+mod m20230218_135537_index;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20230113_000001_init::Migration)]
+        vec![
+            Box::new(m20230113_000001_init::Migration),
+            Box::new(m20230218_135537_index::Migration),
+        ]
     }
+}
+
+pub fn index_name<'a>(table: &'a dyn Iden, col: &'a dyn Iden) -> String {
+    format!(
+        "idx-{table_string}-{col_string}",
+        table_string = table.to_string(),
+        col_string = col.to_string()
+    )
 }
---
+++ src-tauri/src/migrator/m20230218_135537_index.rs
@@ -0,0 +1,181 @@
+use sea_orm_migration::prelude::*;
+
+use crate::migrator::index_name;
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_index(
+                Index::create()
+                    .name(&index_name(&Ingredient::Table, &Ingredient::Name))
+                    .table(Ingredient::Table)
+                    .col(Ingredient::Name)
+                    .to_owned(),
+            )
+            .await?;
+        manager
+            .create_index(
+                Index::create()
+                    .name(&index_name(&Recipe::Table, &Recipe::Name))
+                    .table(Recipe::Table)
+                    .col(Recipe::Name)
+                    .to_owned(),
+            )
+            .await?;
+        manager
+            .create_index(
+                Index::create()
+                    .name(&index_name(
+                        &RecipeIngredient::Table,
+                        &RecipeIngredient::Order,
+                    ))
+                    .table(RecipeIngredient::Table)
+                    .col(RecipeIngredient::Order)
+                    .to_owned(),
+            )
+            .await?;
+        manager
+            .create_index(
+                Index::create()
+                    .name(&index_name(
+                        &RecipeIngredient::Table,
+                        &RecipeIngredient::RecipeStepId,
+                    ))
+                    .table(RecipeIngredient::Table)
+                    .col(RecipeIngredient::RecipeStepId)
+                    .to_owned(),
+            )
+            .await?;
+        manager
+            .create_index(
+                Index::create()
+                    .name(&index_name(
+                        &RecipeIngredient::Table,
+                        &RecipeIngredient::IngredientId,
+                    ))
+                    .table(RecipeIngredient::Table)
+                    .col(RecipeIngredient::IngredientId)
+                    .to_owned(),
+            )
+            .await?;
+        manager
+            .create_index(
+                Index::create()
+                    .name(&index_name(&RecipeStep::Table, &RecipeStep::Order))
+                    .table(RecipeStep::Table)
+                    .col(RecipeStep::Order)
+                    .to_owned(),
+            )
+            .await?;
+        manager
+            .create_index(
+                Index::create()
+                    .name(&index_name(&RecipeStep::Table, &RecipeStep::RecipeId))
+                    .table(RecipeStep::Table)
+                    .col(RecipeStep::RecipeId)
+                    .to_owned(),
+            )
+            .await?;
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_index(
+                Index::drop()
+                    .name(&index_name(&Ingredient::Table, &Ingredient::Name))
+                    .table(Ingredient::Table)
+                    .to_owned(),
+            )
+            .await?;
+        manager
+            .drop_index(
+                Index::drop()
+                    .name(&index_name(&Recipe::Table, &Recipe::Name))
+                    .table(Recipe::Table)
+                    .to_owned(),
+            )
+            .await?;
+        manager
+            .drop_index(
+                Index::drop()
+                    .name(&index_name(
+                        &RecipeIngredient::Table,
+                        &RecipeIngredient::Order,
+                    ))
+                    .table(RecipeIngredient::Table)
+                    .to_owned(),
+            )
+            .await?;
+        manager
+            .drop_index(
+                Index::drop()
+                    .name(&index_name(
+                        &RecipeIngredient::Table,
+                        &RecipeIngredient::RecipeStepId,
+                    ))
+                    .table(RecipeIngredient::Table)
+                    .to_owned(),
+            )
+            .await?;
+        manager
+            .drop_index(
+                Index::drop()
+                    .name(&index_name(
+                        &RecipeIngredient::Table,
+                        &RecipeIngredient::IngredientId,
+                    ))
+                    .table(RecipeIngredient::Table)
+                    .to_owned(),
+            )
+            .await?;
+        manager
+            .drop_index(
+                Index::drop()
+                    .name(&index_name(&RecipeStep::Table, &RecipeStep::Order))
+                    .table(RecipeStep::Table)
+                    .to_owned(),
+            )
+            .await?;
+        manager
+            .drop_index(
+                Index::drop()
+                    .name(&index_name(&RecipeStep::Table, &RecipeStep::RecipeId))
+                    .table(RecipeStep::Table)
+                    .to_owned(),
+            )
+            .await?;
+        Ok(())
+    }
+}
+
+#[derive(Iden)]
+enum Ingredient {
+    Table,
+    Name,
+}
+
+#[derive(Iden)]
+enum Recipe {
+    Table,
+    Name,
+}
+
+#[derive(Iden)]
+enum RecipeIngredient {
+    Table,
+    Order,
+    RecipeStepId,
+    IngredientId,
+}
+
+#[derive(Iden)]
+enum RecipeStep {
+    Table,
+    Order,
+    RecipeId,
+}
