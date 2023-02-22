774c71ed12fbe8b4868d8b3ca88be9ad2b0adb8d
Thomas Gauges <thomas.gauges@gmail.com>
2023-02-22 20:48:06 +0100

    refactor: do not use cfg! macro but conditional compilation when building app data directory but remove mutability of dir variable in release build

--- src-tauri/src/path.rs
+++ src-tauri/src/path.rs
@@ -2,16 +2,30 @@ use std::{fs::create_dir_all, path::PathBuf};
 
 use crate::get_app_handle;
 
-pub fn app_data_dir() -> PathBuf {
-    let mut dir = match get_app_handle().path_resolver().app_data_dir() {
-        Some(some) => some,
-        None => panic!("Could not get data directory."),
+macro_rules! mutable_or_immutable {
+    ($var_name:ident, $var_value:expr) => {
+        #[cfg(any(debug_assertions, test))]
+        let mut $var_name = $var_value;
+
+        #[cfg(not(any(debug_assertions, test)))]
+        let $var_name = $var_value;
     };
-    if cfg!(debug_assertions) || cfg!(test) {
-        let env = match cfg!(test) {
-            true => ".TEST",
-            false => ".DEVELOPMENT",
-        };
+}
+
+pub fn app_data_dir() -> PathBuf {
+    mutable_or_immutable!(
+        dir,
+        match get_app_handle().path_resolver().app_data_dir() {
+            Some(some) => some,
+            None => panic!("Could not get data directory."),
+        }
+    );
+    #[cfg(any(debug_assertions, test))]
+    {
+        #[cfg(test)]
+        let env = ".TEST";
+        #[cfg(not(test))]
+        let env = ".DEVELOPMENT";
         dir.push(env);
     }
     if let Err(err) = create_dir_all(&dir) {
