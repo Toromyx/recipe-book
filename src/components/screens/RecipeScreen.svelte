84a865d0ce4b82a80978e40f740fad263e296baa
Thomas Gauges <thomas.gauges@gmail.com>
2023-03-06 22:24:25 +0100

    feat: implement wake lock for recipe screen

--- src/components/screens/RecipeScreen.svelte
+++ src/components/screens/RecipeScreen.svelte
@@ -1,7 +1,16 @@
 <script>
+  import { onDestroy } from "svelte";
   import RecipeFull from "../content/entity/recipe/RecipeFull.svelte";
 
   export let params;
+
+  const wakeLockSentinelPromise = navigator.wakeLock.request();
+
+  onDestroy(() => {
+    void wakeLockSentinelPromise.then((wakeLockSentinel) =>
+      wakeLockSentinel.release(),
+    );
+  });
 </script>
 
 <div>
