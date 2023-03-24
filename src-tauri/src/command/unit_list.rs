4d054e7711c03a7ca2bb13ff8def23637ad31eb9
Thomas Gauges <thomas.gauges@gmail.com>
2023-03-25 00:03:17 +0100

    fix: filter recipe ingredients without a unit when getting unit list

--- src-tauri/src/command/unit_list.rs
+++ src-tauri/src/command/unit_list.rs
@@ -1,4 +1,4 @@
-use sea_orm::{EntityTrait, QuerySelect};
+use sea_orm::{sea_query::Expr, EntityTrait, QueryFilter, QuerySelect};
 
 use crate::{command::error::CommandError, database, entity::recipe_ingredient};
 
@@ -8,6 +8,7 @@ pub async fn unit_list_get() -> Result<Vec<String>, CommandError> {
     let result = recipe_ingredient::Entity::find()
         .select_only()
         .distinct()
+        .filter(Expr::col(recipe_ingredient::Column::Unit).is_not_null())
         .column(recipe_ingredient::Column::Unit)
         .into_tuple::<String>()
         .all(db)
