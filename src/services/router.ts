29fb47df199c6fa37dd415267c953f1ab54b2965
Thomas Gauges <thomas.gauges@gmail.com>
2023-04-20 21:55:15 +0200

    refactor: automatically escape route parameters when constructing router paths

--- src/services/router.ts
+++ src/services/router.ts
@@ -2,13 +2,24 @@ import type { ComponentType } from "svelte";
 import RecipeListScreen from "../components/screens/RecipeListScreen.svelte";
 import RecipeScreen from "../components/screens/RecipeScreen.svelte";
 
-export const recipeListRoute = () => "/";
-export const recipeRoute = (id: number | string) => `/recipe/${id}`;
+function maybeEscape<T extends boolean | number | string>(
+  component: T,
+  doEscape: boolean,
+): string | T {
+  return doEscape ? encodeURIComponent(component) : component;
+}
+
+export function recipeListRoute() {
+  return "/";
+}
+export function recipeRoute(id: number | string, doEscape = true) {
+  return `/recipe/${maybeEscape(id, doEscape)}`;
+}
 
 /**
  * This object contains all the app's routes for constructing the router.
  */
 export const routes: { [route: string]: ComponentType } = {
   [recipeListRoute()]: RecipeListScreen,
-  [recipeRoute(":id")]: RecipeScreen,
+  [recipeRoute(":id", false)]: RecipeScreen,
 };
