57630e814735351ecf85c33113884b1b03bb4926
Thomas Gauges <thomas.gauges@gmail.com>
2023-12-15 21:37:53 +0100

    update: update npm dependencies, adjust TypeScript configuration to work with new dependencies

--- src/services/polyfill/regex-escaping.ts
+++ src/services/polyfill/regex-escaping.ts
@@ -8,12 +8,11 @@ if (!RegExp.escape) {
   };
 }
 
-declare global {
-  interface RegExpConstructor {
-    /**
-     * Escape a string for usage in regular expressions.
-     * @see https://github.com/tc39/proposal-regex-escaping/blob/main/polyfill.js
-     */
-    escape(string: string): string;
-  }
+// eslint-disable-next-line @typescript-eslint/no-unused-vars -- This interface is used implicitly.
+interface RegExpConstructor {
+  /**
+   * Escape a string for usage in regular expressions.
+   * @see https://github.com/tc39/proposal-regex-escaping/blob/main/polyfill.js
+   */
+  escape(string: string): string;
 }
